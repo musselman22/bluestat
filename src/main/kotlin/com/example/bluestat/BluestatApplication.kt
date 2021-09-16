ff5506eb23be5c79c7a1236785fb33eea3dfc07e
Jacob Musselman <musselmj@oregonstate.edu>
2021-09-16 15:41:11 -0700

    first commit

---
+++ src/main/kotlin/com/example/bluestat/BluestatApplication.kt
@@ -0,0 +1,11 @@
+package com.example.bluestat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BluestatApplication
+
+fun main(args: Array<String>) {
+	runApplication<BluestatApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/bluestat/BluestatApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.bluestat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BluestatApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
